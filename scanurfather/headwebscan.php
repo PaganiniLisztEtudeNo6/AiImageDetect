9b2171f3467c74ea1462db1c62ddd5069556d35d
xcutioner912 <krolock003@gmail.com>
2024-08-24 00:11:30 +0700

    changeshead

--- scanurfather/headwebscan.php
+++ scanurfather/headwebscan.php
@@ -49,10 +49,10 @@
                                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                                 <button class="btn btn-outline-success" type="submit">Search</button>
                             </form>
-                            </div>
                         </div>
                     </div>
-                </nav>
+                </div>
+            </nav>
         </div>      
     </div>
 </nav>
