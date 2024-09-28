bb91706ca6770da8f87fd36f5346e7e194c9f727
xcutioner912 <krolock003@gmail.com>
2024-09-28 10:35:22 +0700

    test

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,10 +1,20 @@
 // Home.js
 import UploadImage from "./UploadImage";
-import { Container } from "@mui/material";
+import { Container, AppBar , Toolbar, Typography, Button } from "@mui/material";
 
 export default function Home() {
   return (
     <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            My App
+          </Typography>
+          <Button color="inherit">Home</Button>
+          <Button color="inherit">About</Button>
+          <Button color="inherit">Contact</Button>
+        </Toolbar>
+      </AppBar>
       <Container>
         <div className="h-screen flex items-center justify-center">
           <div className=" px-8 py-8 bg-white border-0 shadow-lg sm:rounded-3xl justify-center">
