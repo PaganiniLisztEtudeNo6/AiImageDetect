8e27c6bc9321b2c6ee9847b8a0a109d8a00f9abc
Donovan <teeseeker1015@gmail.com>
2024-09-30 22:35:12 +0700

    update : detail

--- frontend/src/pages/detail.tsx
+++ frontend/src/pages/detail.tsx
@@ -1,31 +1,26 @@
-
 import { useLocation } from "react-router-dom";
 import "./style.css";
 import { Container, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 
-export default function transfer() {
-
+export default function Transfer() {
   const location = useLocation();
-  const { detail , img } = location.state || {};
+  const { detail, img } = location.state || {};
   const [imageUrl, setImageUrl] = useState<string | null>(null);
 
-  const convertBase64ToBlob = (base64Image : string) => {
+  const convertBase64ToBlob = (base64Image: string) => {
     const mime = "image/png";
     return base64ToBlob(base64Image, mime);
   };
 
-  const imageBlob = img ? convertBase64ToBlob(`data:image/png;base64,${img}`) : null;
-
-
-  function base64ToBlob(base64 : string, mime : string)  { 
+  function base64ToBlob(base64: string, mime: string) {
     const byteString = atob(base64.split(",")[1]);
     const ab = new ArrayBuffer(byteString.length);
     const ia = new Uint8Array(ab);
-    for (let i = 0; i < byteString.length; i++) { 
+    for (let i = 0; i < byteString.length; i++) {
       ia[i] = byteString.charCodeAt(i);
     }
-    return new Blob([ab] , {type:mime});
+    return new Blob([ab], { type: mime });
   }
 
   useEffect(() => {
@@ -34,32 +29,32 @@ export default function transfer() {
       const imageUrl = URL.createObjectURL(imageBlob);
       setImageUrl(imageUrl);
     }
-  },[img]);
+  }, [img, convertBase64ToBlob]);
 
   return (
     <div>
       <Container>
         <div className="h-screen flex items-center justify-center">
-          <div className="w-96 px-14 py-24 bg-white border-0 shadow-lg sm:rounded-3x1">
+          <div className="w-full px-6 py-8 bg-gray-50 border border-gray-200 shadow-xl rounded-lg">
             <Typography variant="h4" component="h1" gutterBottom align="center">
-              Sunny handsome
-            
+              Sunny Handsome
             </Typography>
 
-            <div className="mt-4">
-              <Typography variant="body1">
-                  <strong>Detail:</strong> {JSON.stringify(detail, null, 2)}
+            <div className="mt-4 text-center">
+              <Typography variant="body1" className="text-gray-700">
+                <strong>Detail:</strong> {JSON.stringify(detail, null, 2)}
               </Typography>
             </div>
 
             {imageUrl && (
               <div className="mt-4">
-                <img src={imageUrl} alt="Uploaded" className="w-full h-auto"></img>
-
+                <img
+                  src={imageUrl}
+                  alt="Uploaded"
+                  className="w-full h-48 object-cover rounded-lg shadow-md"
+                />
               </div>
             )}
-            
-
           </div>
         </div>
       </Container>
